fdfaed92ff57310f36cbb11db0f830d184b608e1
Shamnas Koyani <shamnas@loyalitsolutions.com>
2023-11-30 17:24:00 +0530

    Added error handling

--- app.js
+++ app.js
@@ -9,24 +9,36 @@ const doneFolder = './done'; // Folder to move printed files
 FileSystem.mkdirSync(watchFolder, { recursive: true });
 FileSystem.mkdirSync(doneFolder, { recursive: true });
 
-// Function to print and move file
-function printAndMoveFile(file) {
-  try {
-    const filePath = path.join(watchFolder, file);
-    const command = process.platform === 'win32' ? `notepad /p ${filePath}` : `lp ${filePath}`;
+// Function to print file
+function printFile(filePath) {
+  return new Promise((resolve, reject) => {
+    const command = process.platform === 'win32' ? `notepad /p "${filePath}"` : `lp "${filePath}"`;
 
     exec(command, (error) => {
       if (error) {
-        console.error(`Error printing ${file}:`, error);
+        console.error(`Error printing file: ${filePath}`, error);
+        reject(error);
         return;
       }
 
-      console.log(`Printed ${file}`);
-
-      const targetPath = path.join(doneFolder, file);
-      FileSystem.renameSync(filePath, targetPath);
-      console.log(`Moved ${file} to 'done' folder`);
+      console.log(`Printed ${filePath}`);
+      resolve();
     });
+  });
+}
+
+// Function to print and move file
+async function printAndMoveFile(file) {
+  try {
+    const filePath = path.join(watchFolder, file);
+
+    // Print the file
+    await printFile(filePath);
+
+    // Move the file
+    const targetPath = path.join(doneFolder, file);
+    FileSystem.renameSync(filePath, targetPath);
+    console.log(`Moved ${file} to 'done' folder`);
   } catch (error) {
     console.error(`Error handling ${file}:`, error);
   }
